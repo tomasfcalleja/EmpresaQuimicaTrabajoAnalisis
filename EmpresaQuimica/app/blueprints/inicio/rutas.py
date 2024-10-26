a029d0b1a2f8e1c9a8e88aea798a8a4617f8f183
tomasfcalleja <tomasfcalleja@gmail.com>
2024-10-26 14:58:24 -0300

    Agrega funcionalidad carrrito

--- EmpresaQuimica/app/blueprints/inicio/rutas.py
+++ EmpresaQuimica/app/blueprints/inicio/rutas.py
@@ -1,7 +1,28 @@
 from flask import Blueprint, render_template
+from app.services.productos_service import ProductoService
 
 inicio_bp = Blueprint('inicio', __name__)
 
 @inicio_bp.route('/inicio')
 def inicio():
-    return render_template('inicio/inicio.html')
+    productos = ProductoService.obtener_productos()  
+    return render_template('inicio/inicio.html', productos=productos)
+
+from flask import Blueprint, render_template
+from app.services.productos_service import ProductoService
+
+inicio_bp = Blueprint('inicio', __name__)
+
+@inicio_bp.route('/inicio')
+def inicio():
+    productos = ProductoService.obtener_productos()  
+    return render_template('inicio/inicio.html', productos=productos)
+
+@inicio_bp.route('/ver_carrito')
+def ver_carrito():
+    return render_template('inicio/ver_carrito.html')
+
+
+@inicio_bp.route('/realizar_pago')
+def realizar_pago():
+    return render_template('inicio/realizar_pago.html')
